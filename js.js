8b8da17619f3a6d189e2e91e40418d052fbf5b18
KillRealBadBoy <60619760+KillRealBadBoy@users.noreply.github.com>
2020-02-11 23:07:24 +0300

    With use FileReader

--- js.js
+++ js.js
@@ -1,4 +1,69 @@
-const input = document.querySelector('input[type="file"]')
+function dropHandler(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  // FileList object.
+  var files = evt.dataTransfer.files;
+
+  if (files.length != 1) {
+      resetLog();
+      appendLog("Please drag and drop 1 file!");
+      return;
+  }
+  var file = files[0];
+
+  var fileReader = new FileReader();
+
+  fileReader.onloadstart = function(progressEvent) {
+      resetLog();
+      appendLog("onloadstart!");
+      var msg = "File Name: " + file.name + "<br>" +
+          "File Size: " + file.size + "<br>" +
+          "File Type: " + file.type;
+
+      appendLog(msg);
+  }
+
+  fileReader.onload = function(progressEvent) {
+      appendLog("onload!");
+
+      var stringData = fileReader.result;
+      appendLog(" ---------------- File Content ----------------: ");
+      appendLog(stringData);
+  }
+
+  fileReader.onloadend = function(progressEvent) {
+      appendLog("onloadend!");
+      // FileReader.EMPTY, FileReader.LOADING, FileReader.DONE
+      appendLog("readyState = " + fileReader.readyState);
+  }
+
+  fileReader.onerror = function(progressEvent) {
+      appendLog("onerror!");
+      appendLog("Has Error!");
+  }
+
+  // Read file asynchronously.
+  fileReader.readAsText(file, "UTF-8"); // fileReader.result -> String.
+}
+
+function dragoverHandler(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  // Explicitly show this is a copy.
+  evt.dataTransfer.dropEffect = 'copy';
+}
+
+function resetLog() {
+  let x = document.getElementById("testInput").value;
+  document.getElementById('log-div').innerHTML = "";
+}
+
+function appendLog(msg) {
+  document.getElementById('log-div').innerHTML += "<br>" + msg;
+}
+
+/*const input = document.querySelector('input[type="file"]')
 input.addEventListener('change'), 
 function handleFiles (files) {
   console.log(files)
@@ -12,26 +77,12 @@ function handleFiles (files) {
     document.body.appendChild(reader)
   }
   reader.readAsText(files[0])
-}, false
+}, 
+function resetLog() {
+  document.getElementById('files').innerHTML = "";
+}
 
- /*$("form[name='uploader']").submit(function(e) {
-        var formData = new FormData($(this)[0]);
-
-        $.ajax({
-            url: 'file.php',
-            type: "POST",
-            data: formData,
-            async: false,
-            success: function (msg) {
-                alert(msg);
-            },
-            error: function(msg) {
-                alert('Ошибка!');
-            },
-            cache: false,
-            contentType: false,
-            processData: false
-        });
-        e.preventDefault();
-    });
-    */
+function appendLog(msg) {
+  document.getElementById('files').innerHTML += "<br>" + msg;
+}
+/*document.getElementById('files').addEventListener('change', handleFileSelect, false)*/
